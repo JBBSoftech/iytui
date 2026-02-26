e45554b303fd0902334b206208cba653ff71adc7
JBBSoftech <jbbsoftech@gmail.com>
2026-02-26 12:23:18 +0530

    Update api_service.dart

--- Appifyours/frontend/lib/services/api_service.dart
+++ Appifyours/frontend/lib/services/api_service.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart'; // For BuildContext
 import 'dart:typed_data'; // For Uint8List
 import 'dart:io' show File;
 import 'package:http_parser/http_parser.dart'; // For MediaType
-import 'package:frontend/config/environment.dart'; // Import environment configuration
+import 'package:appifyours/config/environment.dart'; // Import environment configuration
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'dart:async'; // For StreamController
 
